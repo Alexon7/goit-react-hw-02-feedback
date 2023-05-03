23c4942999711ffa31d222b87af5934f495a3a14
Alexon7 <alenkaalexon@gmail.com>
2023-05-03 09:07:45 +0200

    task-1

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import Section from './components/Section';
+import FeedbackOptions from 'components/FeedbackOptions';
+import Statistics from './components/Statistics';
+import Notification from 'components/Notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = option => {
+    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+  };
+
+  onTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.onTotalFeedback()) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.onTotalFeedback(this.state);
+
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {total === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={this.countPositiveFeedbackPercentage(total)}
+            />
+          )}
+        </Section>
+      </>
+    );
+  }
+}
